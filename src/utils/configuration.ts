4a799e788a844f344563bf2e3a371741a5e66fd6
Dev Vrat Singh <itsdvvs.18@gmail.com>
2021-03-07 20:30:40 +0530

    Implemented function to load desired environment vars

---
+++ src/utils/configuration.ts
@@ -0,0 +1,11 @@
+import dotenv from "dotenv";
+import logger from "./logger";
+
+// loading the configuration file (ie .env).
+export default function loadEnvironment() {
+  logger.info("Environment Active: " + process.env.NODE_ENV);
+
+  process.env.NODE_ENV === "production"
+    ? dotenv.config({ path: ".env" })
+    : dotenv.config({ path: ".env.dev" });
+}
