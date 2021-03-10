f71784a698dea171d7434af2d388b738f64ec53f
Dev Vrat Singh <itsdvvs.18@gmail.com>
2021-03-10 22:57:00 +0530

    Created a controller for CRD operations for Admin, Reader and Publisher

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,13 +1,14 @@
 import { Request, Response } from "express";
-import MongoDB from "mongodb";
 import BCryptJS from "bcryptjs";
 
 import logger from "../utils/logger";
 import { User } from "../models/user.model";
+import { TRANSACTION_TYPE } from "../models/transaction_type.model";
 
 // functions required for the operations.
 import { makeNewEntry } from "../utils/db_operations/create_op";
 import { deleteAnEntry } from "../utils/db_operations/delete_op";
+import { fetchAnEntry, fetchEntries } from "../utils/db_operations/read_op";
 
 // =======================================
 // FOR CREATING A NEW USER.
@@ -24,11 +25,26 @@ function createNewUser(request: Request, response: Response) {
       logger.error("[user.controller, generateHashFrom()] Encryption Failed.");
 
       // database transaction (create operation).
-      const collection_name = userData.role.toLowerCase();
-      makeNewEntry(userData, collection_name).then((success) => {
-        success
-          ? response.status(200).json({ msg: "User creation successfull!" })
-          : response.status(503).json({ msg: "User creation failed!" });
+      const collectionName = userData.role.toLowerCase();
+      makeNewEntry(userData, collectionName).then((transaction) => {
+        switch (transaction.type) {
+          case TRANSACTION_TYPE.SUCCESS:
+            response.status(200).json({
+              msg: "User creation successfull!",
+              data: transaction.data,
+            });
+            break;
+
+          case TRANSACTION_TYPE.FAILURE:
+            response
+              .status(503)
+              .json({ msg: "User creation failed!", data: transaction.data });
+            break;
+
+          default:
+            logger.error("[user.controller.ts, makeNewEntry] Default case.");
+            break;
+        }
       });
     })
     .catch((error) => {
@@ -40,17 +56,120 @@ function createNewUser(request: Request, response: Response) {
 }
 
 // =======================================
+// FOR FETCHING ALL THE USERS INFO (OF SAME ROLE)
+// =======================================
+function getUsers(request: Request, response: Response) {
+  // search criteria. Must NOT contain ID as a filter criteria.
+  const filter = request.body.filter;
+  const collectionName = request.body.collection;
+  const projection: Array<string> = request.body.fields;
+
+  // database transaction (read operation).
+  fetchEntries(filter, collectionName, projection).then((transaction) => {
+    switch (transaction.type) {
+      case TRANSACTION_TYPE.SUCCESS:
+        response.status(200).json({
+          msg: "Users fetched successfully!",
+          data: transaction.data,
+        });
+        break;
+
+      case TRANSACTION_TYPE.NORESULT:
+        response
+          .status(200)
+          .json({ msg: "Users do not exist!", data: transaction.data });
+        break;
+
+      case TRANSACTION_TYPE.FAILURE:
+        response.status(503).json({
+          msg: "Fetching of users failed!",
+          data: transaction.data,
+        });
+        break;
+
+      default:
+        logger.error("[users.controller.ts, makeNewEntry] Default case.");
+        break;
+    }
+  });
+}
+
+// =======================================
+// FOR FETCHING A USER INFO
+// =======================================
+function getThisUser(request: Request, response: Response) {
+  const userID = request.params.id;
+  const collectionName = request.body.collection;
+  const projection: Array<string> = request.body.fields;
+
+  // database transaction (read operation).
+  fetchAnEntry(userID, collectionName, projection).then((transaction) => {
+    switch (transaction.type) {
+      case TRANSACTION_TYPE.SUCCESS:
+        response.status(200).json({
+          msg: "User fetched successfully!",
+          data: transaction.data,
+        });
+        break;
+
+      case TRANSACTION_TYPE.NORESULT:
+        response
+          .status(200)
+          .json({ msg: "User does not exists!", data: transaction.data });
+        break;
+
+      case TRANSACTION_TYPE.FAILURE:
+        response
+          .status(503)
+          .json({ msg: "Fetching user failed!", data: transaction.data });
+        break;
+
+      default:
+        logger.error("[users.controller.ts, makeNewEntry] Default case.");
+        break;
+    }
+  });
+}
+
+// =======================================
 // FOR REMOVING AN EXISTING USER.
 // =======================================
 function removeThisUser(request: Request, response: Response) {
   const userID: string = request.params.id;
-  const role: string = request.body.role;
+  const collectionName: string = request.body.collection;
 
   // database transaction (delete operation).
-  // collection name = user role (in lowercase).
-  deleteAnEntry(userID, role.toLowerCase()).then((success) => {
-    success
-      ? response.status(200).json({ msg: "User removal successfull!" })
-      : response.status(503).json({ msg: "User removal failed!" });
+  deleteAnEntry(userID, collectionName).then((transaction) => {
+    switch (transaction.type) {
+      case TRANSACTION_TYPE.SUCCESS:
+        response.status(200).json({
+          msg: "User removal successfull!",
+          data: transaction.data,
+        });
+        break;
+
+      case TRANSACTION_TYPE.NORESULT:
+        response
+          .status(200)
+          .json({ msg: "User does not exists!", data: transaction.data });
+        break;
+
+      case TRANSACTION_TYPE.FAILURE:
+        response
+          .status(503)
+          .json({ msg: "User creation failed!", data: transaction.data });
+        break;
+
+      default:
+        logger.error(
+          "[user.controller.ts, removeThisUser] Switch default case."
+        );
+        break;
+    }
   });
 }
+
+export { createNewUser };
+export { getUsers };
+export { getThisUser };
+export { removeThisUser };
